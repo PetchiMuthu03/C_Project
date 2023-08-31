25070463e482977d622a62aca67c4c732a823f04
PetchiMuthu03 <143535949+PetchiMuthu03@users.noreply.github.com>
2023-08-31 07:15:39 +0530

    Add files via upload

---
+++ PATTERN6.C
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+   int i,j,n;
+   printf("Enter the number:");
+   scanf("%d",&n);
+   for(i=0;i<=n;i++)
+   {
+       for(j=-n;j<=n;j++)
+       {
+           if(-(n-i)<j&&j<(n-i))
+           {
+               printf(" ");
+           }
+           else
+           {
+               printf("*");
+           }
+       }
+           printf("\n");
+       }
+    return 0;
+}
