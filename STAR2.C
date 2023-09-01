7a6cc2ea3ec27978bceec23507df45a9c15ced29
PetchiMuthu03 <143535949+PetchiMuthu03@users.noreply.github.com>
2023-09-01 23:06:52 +0530

    Add files via upload

---
+++ STAR2.C
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include<conio.h>
+
+int main() {
+    int i,j,l,n,s;
+    printf("Enter the number:");
+    scanf("%d",&n);
+    clrscr();
+    for(i=-n;i<n;i++)
+    {
+	if(i<0)
+	{
+	    l=-(i);
+	}
+	else
+	{
+	    l=i;
+	}
+	for(s=0;s<n-l+1;s++)
+	{
+	    printf(" ");
+	}
+	for(j=0;j<l+1;j++)
+	{
+	    printf("* ");
+	}
+	printf("\n");
+    }
+    getch();
+    return 0;
+}
