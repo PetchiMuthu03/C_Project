8d9ee876b011ed819ff7e7c21e527f0d27089641
PetchiMuthu03 <143535949+PetchiMuthu03@users.noreply.github.com>
2023-09-05 17:22:48 +0530

    Add files via upload

--- STAR7.C
+++ STAR7.C
@@ -1,46 +1,25 @@
 #include <stdio.h>
 
 int main() {
-  int i,j,n,k;
-  printf("Enter the number:");
-  scanf("%d",&n);
-  for(i=1;i<=2*n-1;i++)
-  {
-      if(i<=n)
-      {
-          k=n;
-      for(j=1;j<=2*n-1;j++)
-      {
-          printf("%d",k);
-          if(i>j)
-          {
-              k=k-1;
-          }
-          if((i+j)>=(2*n))
-          {
-              k=k+1;
-          }
-      }
-      printf("\n");
-      }
-      else
-      {
-          k=n;
-          for(j=1;j<=2*n-1;j++)
-        {
-              printf("%d",k);
-              if(i+j<2*n)
-              {
-                  k=k-1;
-              }
-              if(i<=j)
-              {
-                  k=k+1;
-              }
-          }
-          printf("\n");
-      }
-  }
-  
+int i,j,n;
+printf("Enter the number:");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+    for(j=0;j<n;j++)
+    if(i==j)
+    {
+        printf("%2d",i+1);
+    }
+    else if(i+j==n-1)
+    {
+        printf("%2d",j+1);
+    }
+    else
+    {
+        printf(" ");
+    }
+    printf("\n");
+}
     return 0;
 }
