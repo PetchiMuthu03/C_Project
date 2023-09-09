d98ce35078a5e403a07a4a3cb080fbbf36ca9427
PetchiMuthu03 <143535949+PetchiMuthu03@users.noreply.github.com>
2023-09-09 18:33:22 +0530

    Update STARPAT.C

--- STARPAT.C
+++ STARPAT.C
@@ -19,3 +19,14 @@ for(i=0;i<n;i++)
 }
     return 0;
 }
+output:
+Enter the number:9
+*       *       * 
+  *     *     *   
+    *   *   *     
+      * * *       
+* * * * * * * * * 
+      * * *       
+    *   *   *     
+  *     *     *   
+*       *       *
