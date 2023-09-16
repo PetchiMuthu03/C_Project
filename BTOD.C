5ee824e3e17c6fe25b890e5c66ae3fb13f283639
PetchiMuthu03 <143535949+PetchiMuthu03@users.noreply.github.com>
2023-09-16 22:29:16 +0530

    Add files via upload

---
+++ BTOD.C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<math.h>
+
+int main() {
+    int n,rem,dec=0,i;
+    printf("Enter the binary number:");
+    scanf("%d",&n);
+    i=0;
+    while(n!=0)
+    {
+        rem=n%10;
+        dec=dec+rem*pow(2,i);
+        n=n/10;
+        i++;
+    }
+    printf("Decimal number is:%d",dec);
+    return 0;
+}
